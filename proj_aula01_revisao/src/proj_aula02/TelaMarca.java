540e1dd71ac98c1c56cfcd44c3292b1558242690
Aluno Unidade 2 <aluno.un2@grupofametro.com.br>
2026-02-20 11:28:26 -0400

    Aula02_javaFacul_pt4TelaFinal

--- proj_aula01_revisao/src/proj_aula02/TelaMarca.java
+++ proj_aula01_revisao/src/proj_aula02/TelaMarca.java
@@ -1,12 +1,14 @@
 package proj_aula02;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class TelaMarca {
+public class TelaMarca implements ActionListener {
 	
 	JFrame tela = new JFrame("Cadastro de Marca");
 	JPanel painel = new JPanel();
-	JLabel lbnome = new JLabel("nome: ");
+	JLabel lbnome = new JLabel("Nome: ");
 	JLabel lbcodigo = new JLabel("Código: ");
 	JTextField txtcod = new JTextField();
 	JTextField txtnome = new JTextField();
@@ -21,7 +23,7 @@ public class TelaMarca {
 		txtcod.setBounds(110, 10, 100, 30);
 		lbnome.setBounds(10, 50, 100, 30);
 		txtnome.setBounds(110, 50, 100, 30);
-		btsalvar.setBounds(20,100,120,30);
+		btsalvar.setBounds(20,100,120,30); 
 		btcancelar.setBounds(150,100,120,30);
 		
 		painel.setLayout(null);
@@ -30,6 +32,8 @@ public class TelaMarca {
 		painel.add(lbnome);
 		painel.add(txtnome);
 		painel.add(btsalvar);
+		btsalvar.addActionListener(this);
+		
 		painel.add(btcancelar);
 		
 		tela.getContentPane().add(painel);
@@ -40,6 +44,15 @@ public class TelaMarca {
 	public static void main(String[] args) {
 		TelaMarca t=new TelaMarca();
 		t.criarTela();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub 
+		if(e.getSource() == btsalvar)
+			JOptionPane.showMessageDialog(null, "O Código: "+ txtcod.getText()
+		+"\nO nome: "+txtnome.getText());
+		
 	}
 	
 
